77a932737b00d3e93bed6af520fb58f28bf01367
IblD <david.ibl@lv1871.de>
2018-12-17 15:50:44 +0100

    First Commit

---
+++ src/test/java/de/lv1871/dms/JavaLambdaVII/BinaryOperatorTest.java
@@ -0,0 +1,38 @@
+package de.lv1871.dms.JavaLambdaVII;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class BinaryOperatorTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public BinaryOperatorTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( BinaryOperatorTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
